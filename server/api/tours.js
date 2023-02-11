d42d4b42c22e7be1dc3f49b7035e3a10f5aac8b5
Frederick Tetteh <kwesiossom@gmail.com>
2023-02-10 20:16:14 -0500

    "eslint configured"

--- server/api/tours.js
+++ server/api/tours.js
@@ -4,6 +4,18 @@ module.exports = router;
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../../dev-data/data/tours-simple.json`));
 
+
+router.param('id', (req, res, next, val) => {
+    if (val * 1 > tours.length) {
+        return res.status(400).json({
+            status: 'failed',
+            message: 'invalid ID'
+        })
+    }
+    next();
+})
+
+
 router.get('/', (req, res) => {
     res.status(200).json({
         'status': 'success',
@@ -16,14 +28,6 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     const id = req.params.id * 1;
-
-    if (id > tours.length) {
-        return res.status(400).json({
-            status: 'fail',
-            message: 'invalid ID'
-        })
-    }
-
     const tour = tours.find(el => el.id === id);
     res.status(200).json({
         'status': 'success',
@@ -33,13 +37,26 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.post('/', (req, res) => {
-    const newId = tours[tours.length - 1] + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
+const checkBody = (req, res, next) => {
 
+    const { name, price } = req.body;
+    // console.log(`name: ${name} and price: ${price}`)
+
+    if (!name || !price) {
+        return res.status(400).json({
+            status: 'failed',
+            message: 'Values not defined'
+        })
+    }
+    next();
+}
+
+router.post('/', checkBody, (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
     tours.push(newTour);
 
-    fs.writeFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    fs.writeFile(`${__dirname}/../../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
             'status': 'success',
             'results': tours.length,
