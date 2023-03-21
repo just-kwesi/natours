feafad5724c47ad5c001ca27aa20551e808001cd
Frederick Tetteh <kwesiossom@gmail.com>
2023-03-20 22:23:52 -0400

    overview page done

--- server/controllers/viewsController.js
+++ server/controllers/viewsController.js
@@ -1,11 +1,28 @@
-exports.getOverview = (req, res) => {
-  res
-    .status(200)
-    .render('overview', { title: 'Exciting tours for adventurous people' });
+const Tour = require('../db/tourModel');
+
+exports.getOverview = async (req, res, next) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).render('overview', {
+      title: 'Exciting tours for adventurous people',
+      tours,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
-  });
+exports.getTour = async (req, res, next) => {
+  try {
+    const { slug } = req.params;
+    const tour = await Tour.find({ slug });
+
+    res.status(200).render('tour', {
+      title: tour.name,
+      tour,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- server/viewRoutes/index.js
+++ server/viewRoutes/index.js
@@ -5,4 +5,4 @@ const viewController = require('../controllers/viewsController');
 module.exports = router;
 
 router.get('/', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tours/:slug', viewController.getTour);
